6844f9207d86d2d3cac971ed2886234c56ceffae
Will Storey <will@summercat.com>
2013-07-14 12:23:07 -0700

    match re-ordering in signals.h as in signals.c

--- src/signals.h
+++ src/signals.h
@@ -6,15 +6,15 @@
 #include "irssi_includes.h"
 
 void
+time_change(void);
+void
+send_text(char*, SERVER_REC*, WI_ITEM_REC*);
+void
 msg_pub(SERVER_REC*, char*, const char*, const char*,
 	const char*);
 void
 msg_own_pub(SERVER_REC*, char*, char*);
 void
-send_text(char*, SERVER_REC*, WI_ITEM_REC*);
-void
 server_sendmsg(SERVER_REC*, char*, char*, int);
-void
-time_change(void);
 
 #endif
