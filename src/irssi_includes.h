be23f8f9f92475cd0c4a8f2cb3462c8520728ead
Will Storey <will@summercat.com>
2013-07-14 11:28:47 -0700

    remove unnecessary includes

--- src/irssi_includes.h
+++ src/irssi_includes.h
@@ -3,22 +3,23 @@
 #ifndef __IRSSI_INCLUDES_H
 #define __IRSSI_INCLUDES_H
 
-// MODULE_NAME is used in Irssi.
-#define MODULE_NAME "tcl"
+// this is to fix a bug apparently.
+// see github.com/shabble/irssi-scripts test module.
+// another way to silence this seems to be to pass in
+// define HAVE_CONFIG_H..
+#define UOFF_T_LONG_LONG 1
 
 #include <common.h>
-#include <core/servers.h>
+
+// MODULE_NAME is used in Irssi. we need it defined for some of the
+// irssi includes.
+#define MODULE_NAME "tcl"
+
 #include <core/settings.h>
 #include <core/levels.h>
-#include <core/signals.h>
-#include <core/commands.h>
-#include <core/queries.h>
-#include <core/channels.h>
-#include <core/recode.h>
 #include <fe-common/core/printtext.h>
 #include <fe-common/core/window-items.h>
 #include <irc/core/irc.h>
-#include <irc/core/irc-commands.h>
 #include <irc/core/irc-servers.h>
 #include <fe-common/core/module-formats.h>
 #include <fe-common/core/fe-messages.h>
